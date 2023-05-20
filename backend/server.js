4459a74631690bcf7047727797f4f68cfc5c56f8
wasiul <wasiul@melioraweb.com>
2023-05-20 17:08:29 +0600

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
